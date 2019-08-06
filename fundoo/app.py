735c4857bef9ec2acd09512296836fba3557c5ce
LaxmanRaikar <kiranraikar777@gmail.com>
2019-08-06 19:17:17 +0530

    reset password

--- fundoo/app.py
+++ fundoo/app.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://laxman777:password@localhost/fundooproject'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'laxmanraikar777@gmail.com'
+app.config['MAIL_PASSWORD'] = 'laxman123,'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 
 
 
--- fundoo/views.py
+++ fundoo/views.py
@@ -1,9 +1,14 @@
-from app import app
-from app import db
+import self as self
+from app import app,mail
+from app import db,bcrypt
 from flask import jsonify, request, flash
+from flask_mail import Message
 from models import sign_up
 from sqlalchemy import create_engine
+from flask import url_for
+
 from sqlalchemy.orm import sessionmaker, session
+from sqlalchemy.sql.functions import current_user
 
 engine = create_engine('postgresql://laxman777:password@localhost/fundooproject', echo=True)
 from wtforms import PasswordField, StringField, SubmitField, ValidationError
@@ -37,7 +42,7 @@ def SignUp():
             db.session.commit()
 
             response['success'] = True
-            response['message'] = 'successfully feedback has been sent'
+            response['message'] = 'successfully account is created'
             response['data'] = data
             response = jsonify(response)
             return response
@@ -107,6 +112,74 @@ def SignIn():
         response = jsonify(response)
         response.status_code = 400
     return response
+
+
+def send_reset_email(user):
+    """ this method is used to send mail"""
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='laxmanraikar777@gmail.com',
+                  recipients=[user.email_id])
+    msg.body = f'''To reset your password, visit the following link:
+    {url_for('reset_token', token=token, _external=True)}
+    If you did not make this request then simply ignore this email and no changes will be made.
+    '''
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    """ this method is used to reset the password """
+    response = {
+        'success': False,
+        'message': 'something went wrong'
+    }
+    # getting the email id from user to which password should be changed
+    email_id = request.form.get("email_id")
+    # checking the user is is db
+    user = sign_up.query.filter_by(email_id=email_id).first()
+    if user:
+        send_reset_email(user)  # calling the method to send mail
+        response['success'] = True
+        response['message'] = "A link has been sent to your email id to reset password please checkout"
+        return response
+    else:
+        response['success'] = False
+        response['message'] = "this user is not in our database "
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    response = {
+        'success': False,
+        'message': 'Some went wrong',
+    }
+    mypassword = request.form.get('password')
+    confirm_password = request.form.get('confirm')
+    user = sign_up.verify_reset_token(token)
+    myuser = sign_up.query.filter_by(id=user.id).first()
+    if mypassword == confirm_password:
+        myuser.password = mypassword
+        myuser.confirm_password = confirm_password
+        db.session.commit()
+        response['success']=True
+        response['message']="successfully password has changed"
+        return response
+    else:
+        response['success'] = False
+        response['message'] = 'password does not match'
+        return response
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
